b1aee2886e515e69f9d528206b9845c0a914d9f4
Mauro <mauro@insided.nl>
2015-04-05 15:10:04 +0200

    Add fork me in github

--- src/attenders/CristinaValota.js
+++
@@ -1,7 +0,0 @@
-var MauroMandracchia;
-module.exports = MauroMandracchia = new Person({
-  name: 'Cristina',
-  last_name: 'Valota',
-  nationality: 'Italia',
-  description: 'He love food, has much JavaScript'
-});
--- src/dinners/180415-oop_introduction.js
+++ src/dinners/180415-oop_introduction.js
@@ -1,11 +1,9 @@
 var Mauro = require('./../attenders/MauroMandracchia'),
-    Cristina = require('./../attenders/CristinaValota'),
     MontePulcianoWine = require('./../recepies/wine-monte_pulciano'),
     PastaCarbonara = require('./../recepies/pasta-carbonara'),
     DessertTiramisu = require('./../recepies/dessert-tiramisu');
 
 Mauro.brings([ MontePulcianoWine, PastaCarbonara, DessertTiramisu ]);
-// Cristina.brings(MontePulcianoWine);
 
 var Dinner = new Meal({
   title: 'OOP, a Gentle and Tasty Introduction',
