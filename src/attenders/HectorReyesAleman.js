837f45080e603f87f245cbca8850e0625bd99cc4
Hector Reyes-Aleman <birkoff.h@gmail.com>
2015-04-07 18:44:58 +0200

    Adding me

---
+++ src/attenders/HectorReyesAleman.js
@@ -0,0 +1,7 @@
+var HectorReyesAleman;
+module.exports = HectorReyesAleman = new Person({
+    name: 'Hector',
+    last_name: 'Reyes-Aleman',
+    nationality: 'Mexico',
+    description: 'This Js Stuff!'
+});
