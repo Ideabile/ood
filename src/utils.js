47215947eeeae6f5cd3692db4f74c2dde428a78d
Mauro <mauro@insided.nl>
2015-04-07 19:31:55 +0200

    Fix utils from gh-pages

--- src/utils.js
+++ src/utils.js
@@ -135,7 +135,7 @@ var TIME = (typeof window === 'undefined') ? 3000 : 1000,
               });
             });
           });
-          if(praseParsed) this.h1(praseParsed.rest, color, bg, style);
+          if(praseParsed) this.h1(praseParsed.rest, color, bg, style, type);
         },
 
         h2: function(prase, color, bg, style){
@@ -218,7 +218,7 @@ module.exports = Utils = {
         var guest = meal.attributes.guests[z].attributes;
         cli.h2("-----------");
         cli.h1(guest.name+" brings:", 'red');
-        
+
         for(var x in guest.brings){
           var item_guest = guest.brings[x].attributes ? guest.brings[x].attributes.name : "";
           if(typeof item_guest === "string" && item_guest.trim() !== "") cli.h2(item_guest);
