152a1e8e0f74be30b334f690e2f9d3b217fdcfa7
madameFerry <ginalolli@gmail.com>
2015-04-16 17:53:01 +0200

    Update 180415-oop_introduction.js

--- src/dinners/180415-oop_introduction.js
+++ src/dinners/180415-oop_introduction.js
@@ -1,6 +1,7 @@
 var Mauro = require('./../attenders/MauroMandracchia'),
     Hector = require('./../attenders/HectorReyesAleman'),
     Niels = require('./../attenders/NielsDequeker'),
+    Gina = require('./../attenders/GinaLolli'),
     MontePulcianoWine = require('./../recipes/wine-monte_pulciano'),
     PastaCarbonara = require('./../recipes/pasta-carbonara'),
     DessertTiramisu = require('./../recipes/dessert-tiramisu'),
@@ -11,11 +12,13 @@ var Mauro = require('./../attenders/MauroMandracchia'),
     Dick = require('./../attenders/DickBrouwers'),
     Pretzels = require('./../recipes/pretzels'),
     FrenchRedWine = require('./../recipes/wine-red-french');
+    WhiteWine = require('./../recipes/white-wine');
 
 Mauro.brings([ MontePulcianoWine, PastaCarbonara, DessertTiramisu ]);
 Niels.brings([ BeerDuvel, BeerWestmalle, BeerLeffe ]);
 Hector.brings([ Guacamole ]);
 Dick.brings([ Pretzels, FrenchRedWine ]);
+Gina.brings([ WhiteWine]);
 
 var Dinner = new Meal({
   title: 'OOP, a Gentle and Tasty Introduction',
@@ -28,4 +31,5 @@ Dinner.addOwner( Mauro );
 Dinner.addGuest( Niels );
 Dinner.addGuest( Hector );
 Dinner.addGuest( Dick );
+Dinner.addGuest( Gina );
 module.exports = Dinner;
