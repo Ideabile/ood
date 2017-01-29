2b0ff76a8c23164f7d1bf68a248255c23802a112
somur ruteeb <s.ruteeb@gmail.com>
2017-01-29 14:57:30 +0100

    hi

---
+++ src/attenders/AliAtrash.1.js
@@ -0,0 +1,7 @@
+var AliAtrash;
+module.exports = AliAtrash = new Person({
+  name: 'Ali',
+  last_name: 'Atrash',
+  nationality: 'Syria',
+  description: 'Writing code to rule the world. Barely.'
+});
