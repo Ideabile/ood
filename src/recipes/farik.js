f8dfc817da69a6d8953d4349d6e1fbe094525e64
ekhamis <ekhamis@gmail.com>
2017-01-29 15:50:54 +0100

    new dish added

---
+++ src/recipes/farik.js
@@ -0,0 +1,10 @@
+var HomeCookedFarik;
+module.exports = HomeCookedFarik = new Recipe({
+  name: 'Home Cooked Farik',
+  category: 'Fingurelishesh unforgotable food',
+  brand: 'Eyad Kitchen,
+  composition: [
+      new Ingredient({name: 'Farika, Red Meat, Nutts'}),
+      new Manipulation({name: 'Cooked with love'})
+  ]
+});
