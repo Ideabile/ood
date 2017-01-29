214eb2644953a1ce77f9acca74ccdbaa3752b3b8
ekhamis <ekhamis@gmail.com>
2017-01-29 15:14:45 +0100

    Udated file

--- src/dinners/290117-ood-dinner.js
+++ src/dinners/290117-ood-dinner.js
@@ -1,17 +1,20 @@
 var Mauro = require('./../attenders/MauroMandracchia'),
-  Eyad = require('./../attenders/EyadKhamis'),
-  MontePulcianoWine = require('./../recipes/wine-monte_pulciano'),
-  PastaCarbonara = require('./../recipes/pasta-carbonara'),
-  DessertTiramisu = require('./../recipes/dessert-tiramisu');
+    Eyad = require('./../attenders/EyadKhamis'),
+    MontePulcianoWine = require('./../recipes/wine-monte_pulciano'),
+    PastaCarbonara = require('./../recipes/pasta-carbonara'),
+    DessertTiramisu = require('./../recipes/dessert-tiramisu'),
+    SpaBlue = require('./../recipes/spaBlue'),
+    SpaRood = require('./../recipes/spaRood');
+
 
 Mauro.brings([MontePulcianoWine, PastaCarbonara, DessertTiramisu]);
 Eyad.brings([SpaRood, SpaBlue]);
 
 var Dinner = new Meal({
-  title: 'Hack your future OOP',
-  date: '29/01/17',
-  time: '20:30',
-  where: 'Amsterdam'
+    title: 'Hack your future OOP',
+    date: '29/01/17',
+    time: '20:30',
+    where: 'Amsterdam'
 });
 
 Dinner.addOwner(Mauro);
